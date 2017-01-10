68a6b05239b12ce2422f140a1ad906882404b8bc
Mike Ulvila <mike.ulvila@premisehealth.com>
2017-01-10 08:04:07 -0600

    new API version does not need % in url for search

--- javascripts/get-movie-data.js
+++ javascripts/get-movie-data.js
@@ -2,16 +2,17 @@ define(function(require) {
 	var $ = require("jquery");
 	var Q = require("q");
 
-	// this function searches comprehensive data API for all 
-	// movies containing search value (eg. search of 'christmas' 
+	// this function searches comprehensive data API for all
+	// movies containing search value (eg. search of 'christmas'
 	// returns all movies with 'christmas' in the title)
 
 	return {
 		requestData: function(searchValue) {
 
 			var deferred = Q.defer();
-			var parsedSearch = searchValue.replace(" ", "%");
-			
+			//var parsedSearch = searchValue.replace(" ", "%");
+			// new api does not need % in search
+			var parsedSearch = searchValue;
 			var searchURL = "http://www.omdbapi.com/?s=" + parsedSearch + "&";
 			$.ajax({
 			url: searchURL
@@ -22,7 +23,7 @@ define(function(require) {
 			.fail(function(error) {
 				deferred.reject(error);
 			});
-			return deferred.promise;		
+			return deferred.promise;
 		}
 	};
 });
